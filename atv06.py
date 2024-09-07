c06efc3bba87de76d4f8c2560afb3572b76ea987
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-07 18:17:20 -0300

    Nome e sobrenome

---
+++ atv06.py
@@ -0,0 +1,4 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+nome1 =str(input("Digite seu primeiro nome:"))
+nome2 = str(input("Digite seu sobrenome:"))
+print(f"Seu nome completo e {nome1} {nome2}")
